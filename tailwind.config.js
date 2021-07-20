ddadba3cd397d798b889e67ac28ee4ddff273cc4
Railly Hugo <railly_2009@hotmail.com>
2021-07-20 11:46:56 -0500

    Fix tailwind.config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,11 @@
 module.exports = {
   mode: "jit",
-  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  purge: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "/_posts/**/*.mdx",
+    "/lib/**/*.{js, ts, jsx, tsx}",
+  ],
   darkMode: "class", // or 'media' or 'class'
   theme: {
     fontFamily: {
