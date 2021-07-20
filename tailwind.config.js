16450b37c4bc9e6ddc0115be14dd205a0749c9d5
Railly Hugo <railly_2009@hotmail.com>
2021-07-20 14:43:00 -0500

    update tailwind.config

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,8 +3,8 @@ module.exports = {
   purge: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
-    "/_posts/**/*.mdx",
-    "/lib/**/*.{js, ts, jsx, tsx}",
+    "./_posts/**/*.mdx",
+    "./lib/**/*.{js, ts, jsx, tsx}",
   ],
   darkMode: "class", // or 'media' or 'class'
   theme: {
